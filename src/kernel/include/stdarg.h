f021387979b9e3154f6148601e4a02d764fee50b
Matthew Iselin <matthew@theiselins.net>
2011-11-16 17:10:07 +1000

    Fix stdarg.h to use __builtin_va_list instead of char * for the va_list type

--- src/kernel/include/stdarg.h
+++ src/kernel/include/stdarg.h
@@ -1,14 +1,14 @@
 #ifndef _STDARG_H
 #define _STDARG_H
 
-typedef char *va_list;
-
 #ifdef __GNUC__
-#define va_start(ap, pN)	__builtin_va_start(ap, pN) // ((ap) = ((va_list) __builtin_next_arg(pN)))
+typedef __builtin_va_list	va_list;
+#define va_start(ap, pN)	__builtin_va_start(ap, pN)
 #define va_end(ap)			__builtin_va_end(ap)
 #define va_arg(ap, pN)		__builtin_va_arg(ap, pN)
 #define va_copy(d, s)		__builtin_va_copy(d, s)
 #else
+typedef char *va_list;
 #define __va_argsize(t) (((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
 #define va_start(ap, pN) ((ap) = ((va_list) (*pN) + __va_argsize(pN)))
 #define va_end(ap) ((void) 0)
