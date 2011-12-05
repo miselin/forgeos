916f3387231edc6760c3d9b56a17d03e5fc3da55
Rich Edelman <redelman@gmail.com>
2011-12-04 23:30:09 -0600

    Added compiler.h

--- src/kernel/include/compiler.h
+++ src/kernel/include/compiler.h
@@ -17,36 +17,9 @@
 #ifndef _COMPILER_H
 #define _COMPILER_H
 
-#define PACKED		__attribute__((packed))
-#define ALIGN(n)	__attribute__((align(n)))
-#define ALIGNED(n)	__attribute__((aligned(n)))
+#define __packed          __attribute__((packed))
 
-#define SECTION(s)	__attribute__((section(s)))
-#define UNUSED		__attribute__((unused))
+#define STRINGIFY(val)          #val
+#define XSTRINGIFY(val)         STRINGIFY(val)
 
-#define NOP			__asm__ volatile("nop")
-
-#define MEMORY_BARRIER __sync_synchronize()
-
-// Atomicity.
-#define atomic_fetch_and_add	__sync_fetch_and_add
-#define atomic_fetch_and_sub	__sync_fetch_and_sub
-#define atomic_fetch_and_or		__sync_fetch_and_or
-#define atomic_fetch_and_and	__sync_fetch_and_and
-#define atomic_fetch_and_xor	__sync_fetch_and_xor
-#define atomic_fetch_and_nand	__sync_fetch_and_nand
-
-#define atomic_add_and_fetch	__sync_add_and_fetch
-#define atomic_sub_and_fetch	__sync_sub_and_fetch
-#define atomic_or_and_fetch		__sync_or_and_fetch
-#define atomic_and_and_fetch	__sync_and_and_fetch
-#define atomic_xor_and_fetch	__sync_xor_and_fetch
-#define atomic_nand_and_fetch	__sync_nand_and_fetch
-
-#define atomic_bool_compare_and_swap	__sync_bool_compare_and_swap
-#define atomic_val_compare_and_swap		__sync_val_compare_and_swap
-
-#define atomic_lock_test_and_set		__sync_lock_test_and_set
-#define atomic_lock_release				__sync_lock_release
-
-#endif
+#endif /* _COMPILER_H */
