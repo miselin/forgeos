9f09aace3fe74936bc4b947ea4a1cf5a729ead20
Matthew Iselin <matthew@theiselins.net>
2012-09-25 13:39:26 +1000

    scripts: qemu script updated to take arguments on the command line

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -1,2 +1,2 @@
-qemu-system-x86_64 -m 16 -cdrom build/x86-pc/debug/forge.iso -boot d -serial file:build/serial.txt
+qemu-system-x86_64 -m 16 -cdrom build/x86-pc/debug/forge.iso -boot d -serial file:build/serial.txt -monitor stdio $*
 
